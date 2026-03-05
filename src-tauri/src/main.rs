bfde55563628296b3d08fd8b3879668cece3a37c
alaoui <med.sidi.alaoui@gmail.com>
2026-03-05 14:43:38 +0000

    add system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,63 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
+use tauri::{
+    Manager,
+    menu::{MenuBuilder, MenuItemBuilder},
+    tray::{MouseButton, TrayIconBuilder, TrayIconEvent},
+};
 use tauri_plugin_shell::ShellExt;
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .setup(|app| {
+            // Hide the main window on startup
+            if let Some(window) = app.get_webview_window("main") {
+                window.hide().unwrap();
+            }
+
+            // Build tray menu
+            let open = MenuItemBuilder::new("Open Memo").id("open").build(app)?;
+            let quit = MenuItemBuilder::new("Quit").id("quit").build(app)?;
+            let menu = MenuBuilder::new(app).items(&[&open, &quit]).build()?;
+
+            // Build tray icon
+            TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .menu(&menu)
+                .tooltip("Memo")
+                .on_menu_event(|app, event| match event.id().as_ref() {
+                    "open" => {
+                        if let Some(window) = app.get_webview_window("main") {
+                            window.show().unwrap();
+                            window.set_focus().unwrap();
+                        }
+                    }
+                    "quit" => {
+                        app.exit(0);
+                    }
+                    _ => {}
+                })
+                .on_tray_icon_event(|tray, event| {
+                    if let TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        ..
+                    } = event
+                    {
+                        let app = tray.app_handle();
+                        if let Some(window) = app.get_webview_window("main") {
+                            if window.is_visible().unwrap() {
+                                window.hide().unwrap();
+                            } else {
+                                window.show().unwrap();
+                                window.set_focus().unwrap();
+                            }
+                        }
+                    }
+                })
+                .build(app)?;
+
+            // Launch detector on Windows only
             #[cfg(target_os = "windows")]
             {
                 let handle = app.handle().clone();
@@ -20,6 +70,7 @@ fn main() {
                         .unwrap();
                 });
             }
+
             Ok(())
         })
         .run(tauri::generate_context!())
